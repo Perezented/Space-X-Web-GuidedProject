c19416b03e54a634b43ca26e26b2d0adbf16d9ab
Michael Perez <57721734+Perezented@users.noreply.github.com>
2020-04-20 16:39:51 -0500

    Notes from this morning.
    Did not fork the project from the morning.

--- src/components/MissionsList.js
+++ src/components/MissionsList.js
@@ -1,21 +1,36 @@
-import React from "react";
+import React from 'react';
 
-const MissionsList = props => {
-  return (
-    <section className="missions-list">
-      {props.error ? (
-        <div className="error">{props.error}</div>
-      ) : (
-        <div>
-          {props.missions.map(mission => (
-            <div className="mission" key={mission.mission_id}>
-              {mission.mission_name}
-            </div>
-          ))}
-        </div>
-      )}
-    </section>
-  );
+const MissionsList = (props) => {
+    return (
+        <section className="missions-list">
+            {props.error ? (
+                <div className="error">{props.error}</div>
+            ) : (
+                <div>
+                    {props.missions.map((mission) => (
+                        <div
+                            className="mission"
+                            data-testid="missions"
+                            key={mission.mission_id}
+                        >
+                            {mission.mission_name}
+                        </div>
+                    ))}
+                </div>
+            )}
+        </section>
+    );
 };
 
 export default MissionsList;
+/*
+Props states
+
+error
+-empty string (no error)
+-error string
+
+Missions
+-empty array (initial state)
+-array of missions
+*/
